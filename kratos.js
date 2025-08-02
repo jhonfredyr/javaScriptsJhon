799bd897067f03eae9e13fad7a15cad905f855be
lorena <yiselorenasalzar@gmail.com>
2025-08-02 11:03:44 -0500

    subiendo archivos

---
+++ kratos.js
@@ -0,0 +1,12 @@
+let  numero = 69
+console.log("el valor del numero" + numero);
+nombre="jhon fredy"
+edad= "19 años" ;
+console.log(nombre+edad);
+sueldo=4.000;
+console.log(nombre+edad+sueldo); 
+hijosdeseados=2;
+console.log(numero+nombre+edad+sueldo+hijosdeseados);
+let masculino= true;
+let femenino=false
+console.log(" mi genero es ","femenino",femenino,"masculino",masculino);
