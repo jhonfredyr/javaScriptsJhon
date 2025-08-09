c4bbe24f3ad52abd962475987bc780db99c03813
jhonfredyr <riveracjhonf@gmail.com>
2025-08-09 09:19:42 -0500

    operadores

---
+++ fredy.js
@@ -0,0 +1,18 @@
+//string
+var sena =  "ADSO";
+console.log(sena);
+// numeros
+let bumeroEntero=17;
+var numeroNegativo=-4;
+let numeroDecimal=11.74;
+//booleanos true-falce
+var estoyEstudeando = true;
+let estoyAburrido = false;
+//undefine
+var num ;
+console.log(num);
+num = 0;
+console.log(num);
+// null 
+var sinNada = null 
+console.log(sinNada);
---
+++ operadores.js
@@ -0,0 +1,34 @@
+// suma, resta, multiplicacion, division
+// calcular valores de 2 o mas elementos
+let num1 = 4;
+var num2 = 8;
+let suma = num1 + num2;
+console.log("el resultado es " + suma);
+//contadores
+let edad = 24 ;
+edad = edad + 1 ; //edad ++
+console.log("edad es " + edad );
+//resta
+let res1 = 10;
+var res2 = 3 ;
+let resta = res1 - res2 ;
+console.log("el resultado es" + resta)
+//contador resta
+let peras = 25;
+peras = peras - 1 ;//peras--
+console.log("quedan" + peras)
+// multiplicacion
+let nume1 = 3 ;
+var nume2 = 7 ;
+let multiplicacion = nume1 * nume2 ;
+console.log("el resultado es " + multiplicacion )
+// division
+let div1 = 69;
+var div2 = 2 ;
+let division = div1 / div2 ;
+console.log("el resultado es " + division)
+// residuo 
+let resi1 = 23;
+var resi2 = 3
+let residuo = resi1 % resi2 ;
+console.log ( "el residuo es " + residuo )  
