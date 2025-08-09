03f130b197b6121451d39482024d584a165bbd64
jhonfredyr <riveracjhonf@gmail.com>
2025-08-09 11:59:55 -0500

    parse

---
+++ parse.js
@@ -0,0 +1,8 @@
+
+let num1 = prompt ("digite primer numero " );
+let num2 = prompt (" digite el segundo numero");
+num1 = parseInt (num1);
+num2 = parseInt (num2);
+let suma = num1+num2 ;
+console.log(suma);
+alert ( " esta es la suma" + suma );
---
+++ promt.js
@@ -0,0 +1,5 @@
+let palabra1 = prompt ("como vamos vartolo? bien o que ? " );
+let palabra2 = prompt (" aa bueno me alegro");
+let concatenar = palabra1 + palabra2
+console.log(concatenar);
+alert ("esta es mi palabra concatenar " + concatenar );
